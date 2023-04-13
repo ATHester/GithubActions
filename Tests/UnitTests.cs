3955fa859ac62f1c904e68f2da31741f9c9a6703
ATHester <89045713+ATHester@users.noreply.github.com>
2023-04-12 19:51:39 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -29,7 +29,101 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
+        
+        [Test]
+        public void Subtract_Valid_HESTER()
+        {
+            Assert.AreEqual(5, Program.Subtract("8", "5"));
+            Assert.AreEqual(11, Program.Subtract("18","7"));
+            Assert.AreEqual(0, Program.Subtract("12","12"));
+        }
+        
+        [Test]
+        public void Subtract_Invalid_HESTER()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_Null_HESTER()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+        
+        [Test]
+        public void Multiply_Valid_HESTER()
+        {
+            Assert.AreEqual(40, Program.Multiply("8", "5"));
+            Assert.AreEqual(126, Program.Multiply("18","7"));
+            Assert.AreEqual(144, Program.Multiply("12","12"));
+        }
+        
+        [Test]
+        public void Multiply_Invalid_HESTER()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_HESTER()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+        
+        [Test]
+        public void Divide_Valid_HESTER()
+        {
+            Assert.AreEqual(2, Program.Divide("10", "5"));
+            Assert.AreEqual(10, Program.Divide("20","2"));
+            Assert.AreEqual(19, Program.Divide("76","4"));
+        }
+        
+        [Test]
+        public void Divide_Invalid_HESTER()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null_HESTER()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+        
+        [Test]
+        public void Power_Valid_HESTER()
+        {
+            Assert.AreEqual(2, Program.Power("10", "5"));
+            Assert.AreEqual(10, Program.Power("20","2"));
+            Assert.AreEqual(19, Program.Power("76","4"));
+        }
+        
+        [Test]
+        public void Power_Invalid_HESTER()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_Null_HESTER()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
